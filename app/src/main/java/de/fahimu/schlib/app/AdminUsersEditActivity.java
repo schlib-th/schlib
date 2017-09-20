cf631ac4ff37b8371cb1139ece77626e79092c71
Thomas <schlib-th@users.noreply.github.com>
2017-09-20 14:46:04 +0200

    Release Version 1.10: Finished AdminUsersEditActivity, implemented the Spinner and deleting the user account.

--- app/src/main/java/de/fahimu/schlib/app/AdminUsersEditActivity.java
+++ app/src/main/java/de/fahimu/schlib/app/AdminUsersEditActivity.java
@@ -6,6 +6,7 @@
 
 package de.fahimu.schlib.app;
 
+import android.content.Context;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
@@ -15,10 +16,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 
@@ -133,7 +138,7 @@ public final class AdminUsersEditActivity extends SchlibActivity {
 
    private User prevUser, currUser;       // the edited user's attributes onCreate and the current attributes
 
-   private boolean self;                  // true if the edited user equals the logged in admin
+   private boolean self, hasBooks;
 
    private TextView        userDisplay;
    private LendingsAdapter lendingsAdapter;
@@ -158,11 +163,18 @@ public final class AdminUsersEditActivity extends SchlibActivity {
          currUser = User.getNonNull(getIntent().getLongExtra("uid", -1L));
 
          self = Use.getLoggedInNonNull().getUser().equals(prevUser);
+         hasBooks = Lending.getByUser(prevUser, true).size() > 0;
 
          userDisplay = findView(TextView.class, R.id.admin_users_edit_user_display);
          lendingsAdapter = new LendingsAdapter();
          deleteAccount = findView(Button.class, R.id.admin_users_edit_delete_account);
          withdrawIdcard = findView(Button.class, R.id.admin_users_edit_withdraw_idcard);
+
+         SpinnerAdapterListener spinnerAdapterListener = new SpinnerAdapterListener(this);
+         Spinner nBooksSpinner = findView(Spinner.class, R.id.admin_users_edit_nbooks_spinner);
+         nBooksSpinner.setAdapter(spinnerAdapterListener);
+         nBooksSpinner.setSelection(prevUser.getNbooks() - 1);
+         nBooksSpinner.setOnItemSelectedListener(spinnerAdapterListener);
 
          roleGroup = findView(RadioGroup.class, R.id.admin_users_edit_role);
          roleTutor = findView(RadioButton.class, R.id.admin_users_edit_tutor);
@@ -197,7 +209,7 @@ public final class AdminUsersEditActivity extends SchlibActivity {
    private void refreshGUI() {
       User user = currUser;
       userDisplay.setText(App.getStr(R.string.admin_users_edit_display, user.getDisplay(), user.getDisplayIdcard()));
-      deleteAccount.setEnabled(!self);
+      deleteAccount.setEnabled(!self && !hasBooks);
       withdrawIdcard.setEnabled(!self && user.hasIdcard());
    }
 
@@ -205,8 +217,13 @@ public final class AdminUsersEditActivity extends SchlibActivity {
    protected void onPause() {
       try (@SuppressWarnings ("unused") Log.Scope scope = Log.e()) {
          super.onPause();
-         if (currUser.equals(prevUser)) {
-            scope.d("user NOT modified");
+         if (currUser == null) {
+            prevUser.delete();
+            if (prevIdcard != null) {
+               prevIdcard.setLost(true).update();
+            }
+         } else if (currUser.equals(prevUser)) {
+            scope.d("user NOT changed");
          } else {
             currUser.update();
             // set prevIdcard to lost if
@@ -223,55 +240,57 @@ public final class AdminUsersEditActivity extends SchlibActivity {
 
    @Override
    protected void onBarcode(String barcode) {
-      try (@SuppressWarnings ("unused") Log.Scope scope = Log.e()) {
-         Idcard idcard = Idcard.parse(barcode);
-         if (lostDialog != null && lostDialog.isShown) {
-            if (prevIdcard != null && prevIdcard.equals(idcard)) {
-               prevIdcard = null;         // The previous idcard is obviously not lost.
-               lostDialog.toggleButtons();
-            }
-         } else {
-            if (prevIdcard != null && prevIdcard.equals(idcard)) {
-               prevIdcard = null;         // The previous idcard is obviously not lost,
-               lostDialog = null;         // so we don't need to question the user for it.
-            }
-            onBarcode(idcard);
+      Idcard idcard = Idcard.parse(barcode);
+      if (lostDialog != null && lostDialog.isShown) {
+         if (prevIdcard != null && prevIdcard.equals(idcard)) {
+            prevIdcard = null;         // The previous idcard is obviously not lost.
+            lostDialog.toggleButtons();
          }
-         scope.d("****** prevIdcard=" + prevIdcard + ", lostDialog=" + lostDialog + ", curr=" + currUser);
+      } else {
+         if (prevIdcard != null && prevIdcard.equals(idcard)) {
+            prevIdcard = null;         // The previous idcard is obviously not lost,
+            lostDialog = null;         // so we don't need to question the user for it.
+         }
+         onBarcode(idcard);
       }
    }
 
-   private final class LostDialog implements ButtonListener {
+   private final class LostDialog {
       private final NoFocusDialog dialog;
+      private       boolean       isShown;
 
       LostDialog() {
          dialog = new NoFocusDialog(AdminUsersEditActivity.this).activateScannerListener();
-         dialog.setMessage(R.string.dialog_message_admin_users_edit_idcard_lost, prevUser.getDisplayIdcard());
-         dialog.setButton0(R.string.dialog_button0_admin_users_edit_idcard_lost, this);
-         dialog.setButton1(R.string.dialog_button1_admin_users_edit_idcard_lost, this);
       }
 
-      private boolean isShown;
-      private Idcard  currIdcard;
-
-      private void show(Idcard idcard) {
+      private void show(ButtonListener buttonListener) {
          isShown = true;
-         currIdcard = idcard;
+         dialog.setMessage(R.string.dialog_message_admin_users_edit_idcard_lost, prevUser.getDisplayIdcard());
+         dialog.setButton0(R.string.dialog_button0_admin_users_edit_idcard_lost, buttonListener);
+         dialog.setButton1(R.string.dialog_button1_admin_users_edit_idcard_lost, buttonListener);
          dialog.show().setButtonEnabled(1, false);       // show dialog and disable button 1
       }
 
       private void toggleButtons() {
          dialog.setButtonEnabled(0, false).setButtonEnabled(1, true);
       }
+   }
 
-      @Override
-      public void onClick(int id) {
-         lostDialog = null;               // Don't question the user again
-         assignCurrIdcard(currIdcard);
+   private void possiblyShowLostDialogAndRun(@NonNull final Runnable action) {
+      if (lostDialog == null) {
+         action.run();
+      } else {
+         lostDialog.show(new ButtonListener() {
+            @Override
+            public void onClick(int id) {
+               lostDialog = null;            // Don't question the user again
+               action.run();
+            }
+         });
       }
    }
 
-   private void onBarcode(@Nullable Idcard idcard) {
+   private void onBarcode(final @Nullable Idcard idcard) {
       if (idcard == null) {
          showErrorSnackbar(R.string.snackbar_error_not_a_idcard);
       } else if (currUser.hasIdcard() && currUser.getIdcardId() == idcard.getId()) {
@@ -288,34 +307,11 @@ public final class AdminUsersEditActivity extends SchlibActivity {
             Idcard.setStocked(idcard);
          }
          App.playSound(R.raw.bell);
-         assignCurrIdcard(idcard);
+         possiblyShowLostDialogAndRun(new Runnable() {
+            @Override
+            public void run() { reassignIdcard(idcard); }
+         });
       }
-   }
-
-   private void assignCurrIdcard(@Nullable Idcard currIdcard) {
-      if (lostDialog != null) {
-         lostDialog.show(currIdcard);        // first show LostDialog, then on click call this method again
-      } else {
-         currUser.setIdcard(currIdcard);
-         refreshGUI();
-         if (currIdcard != null) {
-            showUndoSnackbar(App.getStr(R.string.snackbar_undo_action), new OnClickListener() {
-               @Override
-               public void onClick(View v) { restorePrevIdcard(); }
-            }, R.string.admin_users_edit_snackbar_undo_idcard_reassigned, currIdcard.getDisplayId());
-         } else {
-            showUndoSnackbar(App.getStr(R.string.snackbar_undo_action), new OnClickListener() {
-               @Override
-               public void onClick(View v) { restorePrevIdcard(); }
-            }, R.string.admin_users_edit_snackbar_undo_idcard_withdrawed);
-         }
-      }
-   }
-
-   private void restorePrevIdcard() {
-      currUser.setIdcard(prevUser.hasIdcard() ? prevUser.getIdcard() : null);
-      refreshGUI();
-      showInfoSnackbar(R.string.admin_users_edit_snackbar_info_idcard_restored);
    }
 
    /* -------------------------------------------------------------------------------------------------------------- */
@@ -324,14 +320,72 @@ public final class AdminUsersEditActivity extends SchlibActivity {
       showInfoSnackbar(R.string.admin_users_edit_snackbar_info_please_scan_idcard);
    }
 
+   private void reassignIdcard(@NonNull Idcard currIdcard) {
+      currUser.setIdcard(currIdcard);
+      refreshGUI();
+      showUndoSnackbar(App.getStr(R.string.snackbar_undo_action), new OnClickListener() {
+         @Override
+         public void onClick(View v) { restorePrevIdcard(); }
+      }, R.string.admin_users_edit_snackbar_undo_idcard_reassigned, currIdcard.getDisplayId());
+   }
+
    public void onWithdrawIdcardClicked(View view) {
-      assignCurrIdcard(null);
+      possiblyShowLostDialogAndRun(new Runnable() {
+         @Override
+         public void run() { withdrawIdcard(); }
+      });
+   }
+
+   private void withdrawIdcard() {
+      currUser.setIdcard(null);
+      refreshGUI();
+      showUndoSnackbar(App.getStr(R.string.snackbar_undo_action), new OnClickListener() {
+         @Override
+         public void onClick(View v) { restorePrevIdcard(); }
+      }, R.string.admin_users_edit_snackbar_undo_idcard_withdrawed);
+   }
+
+   private void restorePrevIdcard() {
+      currUser.setIdcard(prevUser.hasIdcard() ? prevUser.getIdcard() : null);
+      refreshGUI();
+      showInfoSnackbar(R.string.admin_users_edit_snackbar_info_idcard_restored);
    }
 
    public void onDeleteAccountClicked(View view) {
-      try (@SuppressWarnings ("unused") Log.Scope scope = Log.e()) {
-         scope.d("clicked");
+      possiblyShowLostDialogAndRun(new Runnable() {
+         @Override
+         public void run() { deleteAccount(); }
+      });
+   }
+
+   private void deleteAccount() {
+      NoFocusDialog dialog = new NoFocusDialog(this);
+      dialog.setMessage(R.string.dialog_message_admin_users_edit_clicked_deleted, currUser.getDisplay());
+      dialog.setButton0(R.string.app_no, null);
+      dialog.setButton1(R.string.app_yes, new ButtonListener() {
+         @Override
+         public void onClick(int id) { currUser = null; finish(); }
+      }).show();
+   }
+
+   /* -------------------------------------------------------------------------------------------------------------- */
+
+   private final class SpinnerAdapterListener extends ArrayAdapter<String> implements OnItemSelectedListener {
+      SpinnerAdapterListener(@NonNull Context context) {
+         super(context, android.R.layout.simple_spinner_dropdown_item);
+         add(getString(R.string.admin_users_edit_nbooks_book_1));
+         for (int i = 0; i < 4; i++) {
+            add(getString(R.string.admin_users_edit_nbooks_book_n, 2 + i));
+         }
       }
+
+      @Override
+      public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+         currUser.setNbooks(position + 1);
+      }
+
+      @Override
+      public void onNothingSelected(AdapterView<?> parent) {}
    }
 
    /* -------------------------------------------------------------------------------------------------------------- */
